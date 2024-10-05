2224fcc615ea4d190248078caf6df70f364ec6d9
nasoundead <nasoundead@163.com>
2024-10-05 21:57:41 +0800

    rustic

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -1,16 +1,18 @@
 (use-package toml-mode
   :ensure t)
-;; (use-package rustic
-;;   :ensure t
-;;   :init
-;;   (setq rustic-lsp-client 'eglot))
-;; (add-hook 'rust-mode-hook 'eglot-ensure)
 
-(use-package rust-mode
-  :init
-  (setq rust-mode-treesitter-derive t)
-  :hook
-  (rust-mode-hook . eglot-ensure)
-  )
+(use-package rustic
+  :mode ("\\.rs$" . rustic-mode)
+  :config
+  (setq rustic-lsp-client 'eglot)
+  :custom
+  (rustic-analyzer-command '("rustup" "run" "stable" "rust-analyzer")))
+
+;; (use-package rust-mode
+;;   :init
+;;   (setq rust-mode-treesitter-derive t)
+;;   :hook
+;;   (rust-mode-hook . eglot-ensure)
+;; )
 
 (provide 'init-rust)
